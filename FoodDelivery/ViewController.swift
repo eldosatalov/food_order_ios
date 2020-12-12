c8196fb07a0faf08c511dd12fb947a42d9c11075
Eldos Atalov <eldosatalov@gmail.com>
2020-12-12 09:11:21 +0600

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Akira Watanabe on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
